4e72ac64657e5ade65fd5bf9b82f50fcbd5b37bf
domrons <root@pl.biz.net.id>
2020-02-24 15:02:16 +0000

    version 0.0.3

--- web/data-web/stack/index.php
+++ web/data-web/stack/index.php
@@ -11,7 +11,7 @@
             <div class="hero-body">
                 <div class="container has-text-centered">
                     <h1 class="title">
-                        MY STACK version2
+                        MY STACK version3
                     </h1>
                     <h2 class="subtitle">
                         Your local development environment - 

c3de45b6ad5748247ef664dffc1a2e0e2a392622
domrons <root@pl.biz.net.id>
2020-02-24 14:38:02 +0000

    version 0.0.1

---
+++ versioning.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+USERNAME=beckblurry
+IMAGE=php-apache
+
+version=`cat VERSION`
+echo "version: $version"
+
+sudo git pull 
+
+# tag it
+sudo git add -A
+sudo git commit -m "version $version"
+sudo git tag -a "$version" -m "version $version"
+sudo git push
+sudo git push --tags

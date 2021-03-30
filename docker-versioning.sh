9f1a57a49647851a1e5b39a88d5a98ce732481a8
faatsan <rascalbrick@gmail.com>
2021-03-30 13:58:42 +0700

    Update docker-versioning.sh

--- docker-versioning.sh
+++ docker-versioning.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-USERNAME=faatsan
+USERNAME=rascalbrick
 IMAGE=php-apache
 
 version=$(cat VERSION)
@@ -9,18 +9,18 @@ CHECKIMAGE=$(sudo docker images | grep php-apache)
 
 if [ -z $CHECKIMAGE ]; then
 
-        sudo docker build -t beckblurry/php-apache:$version -f web/Dockerfile .
+        sudo docker build -t rascalbrick/php-apache:$version -f web/Dockerfile .
         sudo docker tag $USERNAME/$IMAGE:$version $USERNAME/$IMAGE:latest
         sudo docker push $USERNAME/$IMAGE:$version
         sudo docker push $USERNAME/$IMAGE:latest
 
         sudo docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=tiger -e MYSQL_DATABASE=png -e MYSQL_USER=png -e MYSQL_PASSWORD=png -d mysql:5.7
-        sudo docker run --name php-apache -p 80:80 --link mysql:mysql --link redis:redis -d beckblurry/php-apache
+        sudo docker run --name php-apache -p 80:80 --link mysql:mysql --link redis:redis -d rascalbrick/php-apache
         sudo docker run --name phpmyadmin -p 8280:80 --link mysql:mysql -e PMA_HOST=mysql -e PMA_PORT=3306 -d phpmyadmin/phpmyadmin
 	sudo docker run --name redis -p 6379:6379 -d redis
 
 else
-        sudo docker build -t beckblurry/php-apache:$version -f web/Dockerfile .
+        sudo docker build -t rascalbrick/php-apache:$version -f web/Dockerfile .
         sudo docker tag $USERNAME/$IMAGE:$version $USERNAME/$IMAGE:latest
         sudo docker push $USERNAME/$IMAGE:$version
         sudo docker push $USERNAME/$IMAGE:latest
